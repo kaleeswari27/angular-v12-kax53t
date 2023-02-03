569f17db2223cc04fe20c4123d64462157708d9d
kaleeswari27 <121480938+kaleeswari27@users.noreply.github.com>
2023-02-03 15:16:37 +0530

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LinkstagComponent } from './linkstag/linkstag.component';
+import { ImageComponent } from '../image/image.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ],
+  declarations: [ AppComponent, HelloComponent,LinkstagComponent ,ImageComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/linkstag/linkstag.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linkstag',
+  templateUrl: './linkstag.component.html',
+  styleUrls: ['./linkstag.component.css'],
+})
+export class LinkstagComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
